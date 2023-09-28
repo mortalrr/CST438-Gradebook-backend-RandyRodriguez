441083dd7d43a6b025ee70bfc603f1f81747ec07
Randy Rodriguez <randyrod1234@hotmail.com>
2023-09-28 13:36:52 -0700

    done

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,12 +1,10 @@
 package com.cst438.controllers;
 
-import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -54,56 +52,68 @@ public class AssignmentController {
 		return result;
 	}
 	
+	@GetMapping("/assignment/{id}")
+	public AssignmentDTO getAssignment(@PathVariable("id") int id)  {
+		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email)
+		Assignment a = assignmentRepository.findById(id).orElse(null);
+		if (a==null) {
+			throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "assignment not found "+id);
+		}
+		// check that assignment is for a course of this instructor
+		if (! a.getCourse().getInstructor().equals(instructorEmail)) {
+			throw  new ResponseStatusException( HttpStatus.FORBIDDEN, "not authorized "+id);
+		}
+		AssignmentDTO adto = new AssignmentDTO(a.getId(), a.getName(), a.getDueDate().toString(), a.getCourse().getTitle(), a.getCourse().getCourse_id());
+		return adto;
+
+	}
 	
-	//create assignment
 	@PostMapping("/assignment")
-	public void createAssignment(@RequestBody AssignmentDTO asDTO) {
-	   Assignment as = new Assignment();
-	    as.setName(asDTO.assignmentName());
-	    as.setDueDate(Date.valueOf(asDTO.dueDate()));
-	   
-	   Course course = courseRepository.findById(asDTO.courseId()).orElseThrow (
-			   () -> ResponseStatusException(HttpStatus.NOT_FOUND, "Course doesn't exist"));
-			   
-		as.setCourse(course);
-		assignmentRepository.save(as);
-			   
+	public int createAssignment(@RequestBody AssignmentDTO adto) {
+		// check that course exists and belongs to this instructor
+		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email)
+		Course c = courseRepository.findById(adto.courseId()).orElse(null);
+		if (c==null || ! c.getInstructor().equals(instructorEmail)) {
+			throw  new ResponseStatusException( HttpStatus.BAD_REQUEST, "course id not found or not authorized "+adto.courseId());
+		}
+		// create and save assignment.  Return generated id to client.
+		Assignment a = new Assignment();
+		a.setCourse(c);
+		a.setDueDate( java.sql.Date.valueOf(adto.dueDate()));
+		a.setName(adto.assignmentName());
+		assignmentRepository.save(a);
+		return a.getId();
 	}
 	
-	
-	//update an assignment
-	@PutMapping("/assignment/{Id}")
-	public void updateAssignment(@PathVariable("id") int as_id, @RequestBody AssignmentDTO asDTO) {
-		 Assignment as = assignmentRepository.findById(as_id).orElseThrow (
-				   () -> ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment doesn't exist"));
-	    
-	    as.setName(asDTO.assignmentName());
-	    as.setDueDate(Date.valueOf(asDTO.dueDate()));
-	    
-	    Course course = courseRepository.findById(asDTO.courseId()).orElseThrow (
-				   () -> ResponseStatusException(HttpStatus.NOT_FOUND, "Course doesn't exist"));
-				   
-			as.setCourse(course);
-			assignmentRepository.save(as);
-	    
+	@PutMapping("/assignment/{id}")
+	public void updateAssignment(@PathVariable("id") int id, @RequestBody AssignmentDTO adto) {
+		// check assignment belongs to a course for this instructor
+	    String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email)
+	    Assignment a = assignmentRepository.findById(id).orElse(null);
+	    if (a==null || ! a.getCourse().getInstructor().equals(instructorEmail)) {
+	    	throw  new ResponseStatusException( HttpStatus.NOT_FOUND, "assignment not found or not authorized "+id);
+	    }
+	    a.setDueDate( java.sql.Date.valueOf(adto.dueDate()));
+	    a.setName(adto.assignmentName());
+	    assignmentRepository.save(a);
 	}
 	
-	
-	// delete an assignment
-    @DeleteMapping("/assignment/{assignment_id}")
-    public void updateAssignment(@PathVariable("assign_id") int as_id, @RequestParam ("force") Optional<String> force) {
-    	boolean hasForce = false;
-    	Assignment as = assignmentRepository.findById(as_id).orElseThrow (
-				   () -> ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment doesn't exist"));
-	    
-	    if(force.get().equals("true")) {
-	    	assignmentRepository.delete(as);
-	    	hasForce = true;
-	    
-    } else {
-    	assignmentRepository.findById(as_id).orElseThrow(
-    			() -> ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment has grades"));
-    };
-	
-}
+	@DeleteMapping("/assignment/{id}")
+	public void deleteAssignment(@PathVariable("id") int id, @RequestParam("force") Optional<String> force) {
+		// check assignment belongs to a course for this instructor
+	    String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email)
+	    Assignment a = assignmentRepository.findById(id).orElse(null);
+	    if (a==null) {
+	    	return;
+	    }
+	    if (! a.getCourse().getInstructor().equals(instructorEmail)) {
+	    	throw  new ResponseStatusException( HttpStatus.FORBIDDEN, "not authorized "+id);
+	    }
+	    // does assignment have grades?  if yes, don't delete unless force is specified 
+	    if (a.getAssignmentGrades().size()==0 || force.isPresent()) {
+	    	assignmentRepository.deleteById(id);
+	    } else {
+	    	throw  new ResponseStatusException( HttpStatus.BAD_REQUEST, "assignment has grades ");
+	    }
+	}
 }
